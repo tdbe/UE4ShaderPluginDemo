a30a89acd1c23492ca08ab9d27c704a7d2e87407
Michal Klos <michal.m.klos@gmail.com>
2016-10-05 01:24:52 +0200

    added volumetric shader for experiments

---
+++ Plugins/VolumetricShader/Source/VolumetricShader.Build.cs
@@ -0,0 +1,26 @@
+namespace UnrealBuildTool.Rules
+{
+	public class VolumetricShader : ModuleRules
+	{
+		public VolumetricShader(TargetInfo Target)
+        {
+            PrivateIncludePaths.AddRange(
+                new string[] {
+					"VolumetricShader/Private"
+				}
+                );
+
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+                    "Engine",
+                    "RenderCore",
+                    "ShaderCore",
+                    "RHI"
+				}
+				);
+		}
+	}
+}
